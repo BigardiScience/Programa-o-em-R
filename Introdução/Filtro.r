1c05d235ced970321300c96ea991d9fb3acc4bd1
BigardiScience <guilhermebigardimessias01@gmail.com>
2025-10-30 16:16:49 -0300

    Delete Introdução/Filtro.r

--- Introdução/Filtro.r
+++
@@ -1,7 +0,0 @@
-alunos <- data.frame(
-  nome = c("Pedrinho", "Joãozinho", "Firmino", "Caio"),
-  notas = c(10,4,7,4)
-)
-print(alunos$notas) #aparece horizontal
-print(alunos[][2]) #aparece em vertical
-alunos[alunos$notas <= 7,] #filtragem 
