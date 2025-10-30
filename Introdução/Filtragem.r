9b982666d8e0529215b7164fa1c606697ec0a2fe
BigardiScience <guilhermebigardimessias01@gmail.com>
2025-10-30 16:17:10 -0300

    Update Filtragem.r

--- Introdução/Filtragem.r
+++ Introdução/Filtragem.r
@@ -2,7 +2,9 @@ alunos <- data.frame(
   nome = c("Heitor", "Lucas", "Rodrigo", "Ryan"),
   notas = c(2,4,6,9)
   )
-alunos[alunos$notas <=7,]
+print(alunos$notas) #aparece horizontal
+print(alunos[][2]) #aparece em vertical
+alunos[alunos$notas <=7,] #aparece como tabela
 
 #     nome     notas
 #  Joãozinho     4
