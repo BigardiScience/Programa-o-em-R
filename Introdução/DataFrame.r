3a07024fd319f969854fb4da6fb4b3a9ce551610
BigardiScience <guilhermebigardimessias01@gmail.com>
2025-10-30 15:16:42 -0300

    Create DataFrame.r

---
+++ Introdução/DataFrame.r
@@ -0,0 +1,8 @@
+produtos <- data.frame(
+  frutas = c("Maca", "Pêra", "Banana"),
+  quantidade = c(3,5,20),
+  preco = c("R$",20,"R$",30,"R$",40)
+)
+print(produtos)
+
+#“<- é usado para atribuir valores a objetos; = é usado para nomear argumentos dentro de funções (embora também funcione para atribuição, não é a forma recomendada).”
