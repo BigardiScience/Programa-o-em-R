3c5aaf2cb6b1ca3ade85556807efe960c46507d3
BigardiScience <guilhermebigardimessias01@gmail.com>
2025-10-30 16:16:22 -0300

    Create Filtragem.r

---
+++ Introdução/Filtragem.r
@@ -0,0 +1,10 @@
+alunos <- data.frame(
+  nome = c("Heitor", "Lucas", "Rodrigo", "Ryan"),
+  notas = c(2,4,6,9)
+  )
+alunos[alunos$notas <=7,]
+
+#     nome     notas
+#  Joãozinho     4
+#    Firmino     7
+#       Caio     4
